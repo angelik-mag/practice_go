e7e5827c66185dde58c159a675f963f7aeb5ab19
angelik-mag <angelik.mag@gmail.com>
2023-04-15 06:59:39 -0500

    Nuevo commit

--- mapas/mapas.go
+++ mapas/mapas.go
@@ -6,5 +6,40 @@ func MostrarMapas() {
 
 	paises := make(map[string]string)
 
-	fmt.Println(paises)
+	paises["Mexico"] = "D.F."
+	paises["Colombia"] = "Bogota"
+
+	// fmt.Println(paises)
+	// fmt.Println(paises["Colombia"])
+
+	campeonato := map[string]int{
+		"Barcelona": 39,
+
+		"Real Madrid": 48,
+		"Boca Junior": 41,
+		"America":     51,
+		"Liver":       78,
+	}
+
+	fmt.Println(campeonato)
+
+	// range es un especie de foreche
+	for equipo, puntaje := range campeonato {
+
+		fmt.Printf("\n El equipo %s tiene un puntale de %d ", equipo, puntaje)
+	}
+
+	// eliminar de un mapa
+	delete(campeonato, "Liver")
+	fmt.Println(campeonato)
+
+	// range es un especie de foreche
+	for equipo, puntaje := range campeonato {
+
+		fmt.Printf("\n El equipo %s tiene un puntale de %d ", equipo, puntaje)
+	}
+
+	puntaje, existe := campeonato["Juventus"]
+
+	fmt.Printf("\n El puntaje es %d y el equipo existe = %t ", puntaje, existe)
 }
